ab49e9de0a5521ce7cd3460d6dccd7bbf57f134d
1dylanleray <dleray77@outlook.fr>
2025-02-13 11:27:28 +0100

    feat: add a funny button

--- header/src/Header.js
+++ header/src/Header.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Button from './Button';
+const Header = () =>{
+    const [fontSize, setFontSize] = useState(16);
 
-const Header = () => <h2>Header Micro Frontend</h2>;
+  const handleClick = () => {
+    setFontSize(prevSize => prevSize + 2);
+  };
+    return (
+        <div>
+       <h2 style={{ fontSize: `${fontSize}px` }}>Header Micro Frontend</h2>
+      <Button onClick={handleClick}>Click Me</Button>
+    </div>
+    );
+} ;
 
 export default Header;
